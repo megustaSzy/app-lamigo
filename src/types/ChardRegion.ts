44e7ee5a7232577815bfb7caa6628018656a61ac
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-23 15:42:18 +0700

    feat: add pagination orders

--- src/types/ChardRegion.ts
+++ src/types/ChardRegion.ts
@@ -11,7 +11,18 @@ export interface ReverseGeocodeResponse {
     //  KECAMATAN & KELURAHAN
     city_district?: string;
     suburb?: string;
+
+    // DETAIL TAMBAHAN (Enhanced OSM)
+    road?: string;
+    house_number?: string;
+    amenity?: string;
+    postcode?: string;
+    building?: string;
+    pedestrian?: string;
+    neighbourhood?: string;
+    country?: string;
   };
+  display_name?: string;
 }
 
 export interface Area {
