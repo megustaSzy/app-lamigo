e5a649a9836b9b12a3ba54825b8444538811a2da
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-01-29 10:28:38 +0700

    feat: refactor loading state

--- src/app/pesan/page.tsx
+++ src/app/pesan/page.tsx
@@ -4,9 +4,5 @@ import PesanComponent from "@/components/pesan/PesanComponent";
 export const dynamic = "force-dynamic";
 
 export default function PesanPage() {
-  return (
-    <Suspense fallback={<p className="text-center mt-20">Loading...</p>}>
-      <PesanComponent />
-    </Suspense>
-  );
+  return <PesanComponent />;
 }
